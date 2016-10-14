e79956eee81660d7d2a030e1381dfce6ff250bf8
PiotrusWatson <piotruswatson@gmail.com>
2016-10-15 00:21:17 +0100

    First files

---
+++ src/GameObjects/GameObject.java
@@ -0,0 +1,40 @@
+package GameObjects;
+
+public class GameObject {
+	//A base by which all objects in the game can use and modify
+	
+	private int xCoord;
+	private int yCoord;
+	private byte layer;
+	
+	public int getXCoord(){
+		return xCoord;
+	}
+	public int getYCoord(){
+		return yCoord;
+	}
+	
+	public byte getLayer(){
+		return layer;
+	}
+	
+	public void setXCoord(int xCoord){
+		this.xCoord = xCoord;
+	}
+	
+	public void setYCoord(int yCoord){
+		this.yCoord = yCoord;
+	}
+	
+	public void setLayer(byte layer){
+		this.layer = layer;
+	}
+	
+	public GameObject(int xCoord, int yCoord, byte layer)
+	{
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.layer = layer;
+	}
+
+}
---
+++ src/test/SetupClass.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class SetupClass extends BasicGame {
+
+	public SetupClass(String title) {
+		super(title);
+	}
+	
+
+	public void init(GameContainer container) throws SlickException {
+		
+	}
+
+	public void update(GameContainer container, int delta) throws SlickException {
+		
+	}
+
+	public void render(GameContainer container, Graphics arg1) throws SlickException {
+		
+	}
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new SetupClass("Setup Test"));
+		app.setDisplayMode(800, 600, false);
+		app.start();
+		
+	}
+
+
+
+
+
+
+
+
+}
