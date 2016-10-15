65eed8c73baf1eea97a69fa7e96d0e8a1d09077e
Adam O'Neill <Adam O'Neill>
2016-10-15 12:26:53 +0100

    getters added to map grid

--- src/levelGen/MapGrid.java
+++ src/levelGen/MapGrid.java
@@ -100,19 +100,24 @@ public class MapGrid {
 	}
 		
 
-	public String toString() {
-		String array = "";
-		for(int i = 0; i<=((grid.length)-1); i++){
-			for(int j = 0; j<=((grid[i].length)-1); j++){
-				array += "" + grid[i][j];
-			}
-		}
-		return "Grid height: " + grid.length + " Grid Length: " + grid[0].length + " array: " + array;
-	}
+	//public String toString() {
+		//String array = "";
+		//for(int i = 0; i<=((grid.length)-1); i++){
+			//for(int j = 0; j<=((grid[i].length)-1); j++){
+				//array += "" + grid[i][j];
+			//}
+		//}
+		//return "Grid height: " + grid.length + " Grid Length: " + grid[0].length + " array: " + array;
+	//}
 	
 	public static void main(String[] args){
 		MapGrid map = new MapGrid(4,4);
 		map.generateGrid(4);
 		System.out.println(map);
+	}
+
+
+	public int[][] getGrid() {
+		return grid;
 	}
 }
