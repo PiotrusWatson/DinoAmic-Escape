b18dc6d71a6a6641f38e04580b211562be32ee76
andrew mackay <andrew mackay>
2016-10-16 10:52:39 +0100

    play

--- src/Game/Menu.java
+++ src/Game/Menu.java
@@ -9,15 +9,16 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-//import java.lang.o
+import java.lang.Object;
+
 public class Menu extends BasicGameState{
 	//Font font;
 	public int mouseXPos;
 	public int mouseYPos;
 	public static int id = 0;
 	Image banner;
-	Image play;
-	TrueTypeFont ttf;
+	Image play2;
+	//TrueTypeFont ttf;
 	int windowWidth = Main.width;
 	int windowHeight = Main.height;
 	
@@ -25,9 +26,10 @@ public class Menu extends BasicGameState{
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		banner = new Image("src/res/banner.png");
-		play = new Image("src/res/playButton.png");
+		//play = new Image("src/res/playButton.png");
+		play2 = new Image("src/res/playbuttonclear.png");
 		//font = new Font("Comic Sans", Font.BOLD, 60);
-		//TrueTypeFont ttf = new TrueTypeFont(font, true);
+		//ttf = new TrueTypeFont(java.awt.Font.SANS_SERIF, true);
 		
 	}
 
@@ -50,12 +52,8 @@ public class Menu extends BasicGameState{
 		}
 		//ttf.drawString(32.0f, 32.0f, "PLAY", Color.white);
 		g.setColor(Color.white);
-
-		g.drawString("Play", windowWidth/4, windowHeight/3);
-
-		
-		
-		
+		play2.draw(windowWidth/4, windowHeight/3);
+		//g.drawString("Play", windowWidth/4, windowHeight/3);
 	}
 
 	@Override
